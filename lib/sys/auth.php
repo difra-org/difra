844a252349637303266ca76286e77bda484a7d84
Eduard Kutuev <nap@ajamstudio.com>
2010-09-08 12:41:39 +0400

    added login sessions support

--- lib/sys/auth.php
+++ lib/sys/auth.php
@@ -3,6 +3,8 @@
 class Auth {
 
 	public $logged = false;
+	public $id = null;
+	public $data = null;
 
 	static public function getInstance() {
 
@@ -11,6 +13,8 @@ class Auth {
 	}
 
 	public function __construct() {
+
+		$this->_load();
 	}
 
 	public function getAuthXML( $node ) {
@@ -19,6 +23,55 @@ class Auth {
 		if( !$this->logged ) {
 			$authNode->appendChild( $node->ownerDocument->createElement( 'unauthorized' ) );
 			return false;
+		} else {
+			$subNode = $authNode->appendChild( $node->ownerDocument->createElement( 'authorized' ) );
+			$subNode->setAttribute( 'id', $this->id );
 		}
 	}
+
+	public function login( $id, $data = null ) {
+
+		$this->id = $id;
+		$this->data = $data;
+		$this->logged = true;
+		$this->_save();
+	}
+
+	public function logout() {
+
+		$this->id = $this->data = null;
+		$this->logged = false;
+		$this->_save();
+	}
+
+	private function _save() {
+
+		if( !isset( $_SESSION ) ) {
+			session_start();
+		}
+		if( $this->logged ) {
+			$_SESSION['auth'] = array(
+				'id'	=> $this->id,
+				'data'	=> $this->data
+			);
+		} else {
+			if( isset( $_SESSION['auth'] ) ) {
+				unset( $_SESSION['auth'] );
+			}
+		}
+	}
+
+	private function _load() {
+
+		if( !isset( $_SESSION ) ) {
+			session_start();
+		}
+		if( !isset( $_SESSION['auth'] ) ) {
+			return false;
+		}
+		$this->id   = $_SESSION['auth']['id'];
+		$this->data = $_SESSION['auth']['data'];
+		return $this->logged = true;
+	}
+
 }
