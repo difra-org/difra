ef8375d161192983358160b87c1dd0c12fffe857
Eduard Kutuev <nap@ajamstudio.com>
2010-09-24 22:32:14 +0400

    updated bin/createProject.sh

--- bin/createProject.sh
+++ bin/createProject.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-mkdir branches
-mkdir branches/current
-mkdir branches/current/bin
-mkdir branches/current/externals
-mkdir branches/current/lib
-mkdir branches/current/plugins
-mkdir branches/current/sites
-mkdir tags
-mkdir trunk
-ln -s ../externals/base/lib/sys branches/current/lib/sys
+mkdir bin
+mkdir externals
+mkdir lib
+mkdir plugins
+mkdir sites
+ln -s ../externals/difra/lib/sys lib/sys
 
-svn add branches tags trunk
+svn add bin externals lib plugins sites
 svn ci -m 'Directory tree automatically created'
-cd branches/current/externals
+cd externals
 
 cat > .tmp.ext << EOF
-base		https://svn.cybergaia.net/base
-base-plugins	https://svn.cybergaia.net/base-plugins
+difra		https://svn.cybergaia.net/difra/trunk
+difra-plugins	https://svn.cybergaia.net/difra-plugins
 EOF
 svn propset svn:externals . -F .tmp.ext
 rm .tmp.ext
@@ -25,13 +21,16 @@ rm .tmp.ext
 svn ci -m 'Base framework external created'
 cd ..
 svn up
-svn export externals/base/sites/default sites/default
+svn export externals/difra/sites/default sites/default
 cd sites/default/templates
 rm -rf common
-ln -s ../../../externals/base/sites/default/templates/common
+ln -s ../../../externals/difra/sites/default/templates/common
 cd ../htdocs/js
 rm -rf common
-ln -s ../../../../externals/base/sites/default/templates/js/common
+ln -s ../../../../externals/difra/sites/default/htdocs/js/common
+cd ../css
+rm -rf adm.css
+ln -s ../../../../externals/difra/sites/default/htdocs/css/adm.css
 cd ../../../..
 svn add sites/default
 svn ci -m 'Created default site'
