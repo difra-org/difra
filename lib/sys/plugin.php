e5dd9be0912174842c911b09dfc52a9a7e9f689d
Eduard Kutuev <nap@ajamstudio.com>
2010-12-08 16:33:55 +0300

    Add Pluginclass->dispatch() call

--- lib/sys/plugin.php
+++ lib/sys/plugin.php
@@ -6,8 +6,19 @@ abstract class Plugin {
 	public function getInstance() {
 
 		static $_self = null;
-		return class_name();
 		return $_self ? $_self : $_self = new self;
+	}
+
+	public function dispatch() {
+
+		$class = substr( get_class( $this ), 1 );
+		$method = 'dispatch';
+		if( class_exists( $class ) ) {
+			if( method_exists( $class, $method ) ) {
+				$instance = call_user_func( array( $class, 'getInstance' ) );
+				$instance->dispatch();
+			}
+		}
 	}
 
 	public function fillAdmMenu( $nodeName, $subnodeName, $href ) {
