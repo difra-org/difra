2644fd879a5d056223c1bc9b55ee0b82173996f7
Eduard Kutuev <nap@ajamstudio.com>
2010-12-23 18:59:12 +0300

    Добавлена функция fetchXML, функция fetchLine переименована в fetchRow

--- lib/sys/mysql.php
+++ lib/sys/mysql.php
@@ -110,10 +110,32 @@ final class MySQL {
 		return false;
 	}
 
-	public function fetchLine( $query ) {
+	public function fetchRow( $query ) {
 
 		$data = $this->fetch( $query );
 		return isset( $data[0] ) ? $data[0] : false;
+	}
+
+	public function fetchXML( $node, $query ) {
+
+		$data = $this->fetch( $query );
+		if( empty( $data ) ) {
+			return false;
+		}
+		foreach( $data as $row ) {
+			$subnode = $node->appendChild( $node->ownerDocument->createElement( 'item' ) );
+			foreach( $row as $k => $v ) {
+				if( trim( $v ) and preg_match( '/^(i|s|a|o|d)(.*);/si', $v ) ) { // serialize!
+					$arr = unserialize( $v );
+					$subnode2 = $subnode->appendChild( $node->ownerDocument->createElement( $k ) );
+					foreach( $arr as $k2 => $v2 ) {
+						$subnode2->setAttribute( $k2, $v2 );
+					}
+				} else {
+					$subnode->setAttribute( $k, $v );
+				}
+			}
+		}
 	}
 
 	/**
