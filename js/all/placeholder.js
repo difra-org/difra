486dea7f8d60312352aa13dc1bea5ce1b3ee4be7
Eduard Kutuev <nap@ajamstudio.com>
2011-04-12 00:53:39 +0400

    + Добавлена черновая версия JavaScript-кода, который обеспечит работу аттрибута input/@placeholder в браузерах, которые не поддерживают HTML5

---
+++ js/all/placeholder.js
@@ -0,0 +1,30 @@
+/*
+ 
+ // XXX: Тут должен быть универсальный код, который обеспечит работу аттрибута <input placeholder="... в браузерах, которые не поддерживают HTML5
+ 
+$(document).ready( function() {
+	$('[placeholder]').focus(function() {
+		var input = $(this);
+		if (input.val() == input.attr('placeholder')) {
+			input.val('');
+			input.removeClass('placeholder');
+		}
+	}).blur(function() {
+		var input = $(this);
+		if (input.val() == '' || input.val() == input.attr('placeholder')) {
+			input.addClass('placeholder');
+			input.val(input.attr('placeholder'));
+		}
+	}).blur();
+
+	$('[placeholder]').parents('form').submit(function() {
+		$(this).find('[placeholder]').each(function() {
+			var input = $(this);
+			if (input.val() == input.attr('placeholder')) {
+				input.val('');
+			}
+		})
+	});
+} );
+ 
+ */
