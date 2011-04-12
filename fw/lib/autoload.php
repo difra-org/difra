b88146d747d31b7633dbb333b67f8911e8c83fd5
Eduard Kutuev <nap@ajamstudio.com>
2011-04-12 17:13:48 +0400

    * Рефакторинг класса Autoload

--- fw/lib/autoload.php
+++ fw/lib/autoload.php
@@ -1,13 +1,44 @@
 <?php
 
-function __autoload( $class ) {
+class Autoloader {
 
-	$file = str_replace( '_', '/', strtolower( $class ) ) . '.php';
-	$dir = DIR_ROOT . 'lib/';
-	if( !file_exists( $dir . $file ) ) {
-		$dir .= 'sys/';
+	private $basePath = '';
+	private $paths = array();
+
+	public static function getInstance() {
+
+		static $_instance = null;
+		return $_instance ? $_instance : $_instance = new self;
 	}
-	if( file_exists( $dir . $file ) ) {
-		include_once( $dir . $file );
+
+	public function __construct() {
+
+		$this->basePath = realpath( dirname( __FILE__ ) . '/../..' );
+		$this->paths[] = 'fw/lib';
+	}
+
+	public function search( $name ) {
+
+		foreach( $this->paths as $path ) {
+			if( file_exists( "{$this->basePath}/$path/$name" ) ) {
+				return "{$this->basePath}/$path/$name";
+			}
+		}
+		return false;
+	}
+
+	public function load( $class ) {
+
+		$fileName = str_replace( '_', '/', strtolower( $class ) ) . '.php';
+		if( !$filePath = $this->search( $fileName ) ) {
+			throw new exception( 'Can\'t find class ' . $class );
+		}
+		include_once( $filePath );
 	}
 }
+
+function __autoload( $class ) {
+
+	Autoloader::getInstance()->load( $class );
+}
+
