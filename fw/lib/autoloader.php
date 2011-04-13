71a2f1903ee42431c2fbe579cc39cbb9c75cf60d
Eduard Kutuev <nap@ajamstudio.com>
2011-04-13 18:10:46 +0400

    + Добавлена поддержка spl_autoload_register для PHP 5.3 и выше

--- fw/lib/autoloader.php
+++ fw/lib/autoloader.php
@@ -27,16 +27,29 @@ class Autoloader {
 		return false;
 	}
 
-	public function load( $class ) {
+	public static function load( $class ) {
 
 		$fileName = str_replace( '_', '/', strtolower( $class ) ) . '.php';
-		if( !$filePath = $this->search( $fileName ) ) {
+		if( !$filePath = self::getInstance()->search( $fileName ) ) {
 			throw new exception( 'Can\'t find class ' . $class );
 		}
 		include_once( $filePath );
 	}
+
+	public static function register() {
+
+		if( function_exists( 'spl_autoload_register' ) ) {
+			if( !spl_autoload_register( 'Autoloader::load' ) ) {
+				throw new exception( 'Can\'t register Autoloader' );
+			}
+		}
+	}
 }
 
+// for PHP 5.3 and newer
+Autoloader::register();
+
+// for older PHP, will not be magic after spl_autoload_register if PHP version >= 5.3
 function __autoload( $class ) {
 
 	Autoloader::getInstance()->load( $class );
