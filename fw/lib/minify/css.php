99bb7c9e44413498973e316d35eb3068c84dd3ac
Eduard Kutuev <nap@ajamstudio.com>
2011-04-29 02:17:38 +0400

    * Маленький рефакторинг

--- fw/lib/minify/css.php
+++ fw/lib/minify/css.php
@@ -6,11 +6,7 @@
 			
 			$data = preg_replace( '/\/\*.*?\*\//s', '', $data );	// remove comments
 			$data = preg_replace( '/\s+/', ' ', $data );		// remove replace multiple whitespaces with space
-			$data = preg_replace( '/\s?{\s/', '{', $data );		// remove spaces near {
-			$data = preg_replace( '/\s?}\s/', '}', $data );		// remove spaces near }
-			$data = preg_replace( '/\s?;\s/', ';', $data );		// remove spaces near ;
-			$data = preg_replace( '/\s?:\s/', ':', $data );		// remove spaces near :
-			$data = preg_replace( '/\s?,\s/', ',', $data );		// remove spaces near ,
+			$data = preg_replace( '/\s?([{};:,])\s/', '$1', $data );// remove spaces near some symbols
 			return $data;
 		}
 	}
