ba9dd605028b3cea025f50a84f47034e161115a0
Eduard Kutuev <nap@ajamstudio.com>
2011-04-29 13:50:34 +0400

    * В dev-режиме теперь Minify не делается

--- fw/lib/minify.php
+++ fw/lib/minify.php
@@ -4,6 +4,10 @@ class Minify {
 
 	static public function getInstance( $type ) {
 		
+		// no minify for developers' hosts
+		if( Site::getInstance()->devMode ) {
+			return Minify_None::getInstance();
+		}
 		switch( $type ) {
 			case 'css':
 				return Minify_CSS::getInstance();
