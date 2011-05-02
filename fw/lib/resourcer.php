37d44298a8822ddfa71b0c0b50cb756bc48dd6c7
Eduard Kutuev <nap@ajamstudio.com>
2011-05-03 02:01:00 +0400

    * Докоммичиваю забытый файл

---
+++ fw/lib/resourcer.php
@@ -0,0 +1,21 @@
+<?php
+	
+	class Resourcer {
+		
+		static public function getInstance( $type, $quiet = false ) {
+			
+			switch( $type ) {
+			case 'css':
+				return Resourcer_CSS::getInstance();
+			case 'js':
+				return Resourcer_JS::getInstance();
+			case 'xslt':
+				return Resourcer_XSLT::getInstance();
+			default:
+				if( !$quiet ) {
+					throw new exception( "Resourcer does not support resource type '$type'" );
+				}
+			}
+		}
+		
+	}
