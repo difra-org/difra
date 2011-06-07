2f52fde5b54bd74f97c6bb312a73268158bee0c8
pnd <pnd@a-jam.ru>
2011-06-07 20:19:06 +0000

    Добавление библиотечки для работы с cookies

---
+++ fw/lib/cookies.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Cookies
+ * @desc Работа с куками
+ * @package fw
+ * @version 0.1 
+ * @access public
+ */
+class Cookies {
+    
+    private $expireTime = null;
+    private $domain = null;
+    private $path = null;
+    
+	/**
+	 * Cookies::getInstance()
+	 * @desc Синглтон
+	 * @return Cookies
+	 */
+	static public function getInstance() {
+		static $_instance = null;
+		return $_instance ? $_instance : $_instance = new self;
+	}
+    
+    /**
+     * Cookies::__construct()
+     * @return void
+     */
+    private function __construct() {
+        $this->expireTime = 0;
+        //$this->domain = Site::getInstance()->getHost();
+        $this->domain = $_SERVER['HTTP_HOST'];
+        if( strstr( $this->domain, 'www.' ) !== false ) {
+            $this->domain = str_replace( 'www.', '.', $this->domain );
+        } else {
+            $this->domain = '.' . $this->domain; 
+        }
+        $this->path = '/';
+    }
+    
+    /**
+     * Cookies::setPath()
+     * @desc Устанавливает путь
+     * @param string $path
+     * @return void
+     */
+    public function setPath( $path ) {
+        $this->path = $path;
+    }
+    
+    /**
+     * Cookies::setDomain()
+     * @desc Устанавливает домен
+     * @param string $domain
+     * @return void
+     */
+    public function setDomain( $domain ) {
+        $this->domain = $domain;
+    }
+    
+    /**
+     * Cookies::setExpire()
+     * @desc Устанавливает время жизни
+     * @param integer $expireTime
+     * @return void
+     */
+    public function setExpire( $expireTime ) {
+        $this->expireTime = $expireTime;  
+    }
+    
+    /**
+     * Cookies::set()
+     * @desc Устанавливает куку
+     * @param string $name
+     * @param string || array $value
+     * @return boolean
+     */
+    public function set( $name, $value ) {
+        if( is_array( $value ) ) {
+            $value = json_encode( $value );
+        }
+        return setrawcookie( $name, rawurlencode( $value ), $this->expireTime, $this->path );
+    }
+
+    /**
+     * Cookies::remove()
+     * @desc Удаляет куку
+     * @param string $name
+     * @return boolean
+     */
+    public function remove( $name ) {
+        return setcookie( $name, '', time() - 108000, $this->path );
+    }
+}
+
