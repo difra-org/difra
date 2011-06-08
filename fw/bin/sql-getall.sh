b5738002bb9a3d1e512d604c50a2abb7553edb7a
Eduard Kutuev <nap@ajamstudio.com>
2011-06-08 20:08:23 +0400

    Скрипт создания таблиц доработан, чтобы брать таблицы из DIR_ROOT/bin/db.sql

--- fw/bin/sql-getall.sh
+++ fw/bin/sql-getall.sh
@@ -3,6 +3,13 @@
 BASEPATH=`dirname $0`/../..
 PLUGINS=$BASEPATH/plugins
 
+name="$BASEPATH/bin/db.sql";
+if [ -f $name ]; then
+	cat $name
+	echo ""
+fi
+
+
 for plugin in `ls $PLUGINS`; do
 	name="$PLUGINS/$plugin/bin/db.sql";
 	if [ -f $name ]; then
