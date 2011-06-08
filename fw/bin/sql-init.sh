457b4e652a382a02709e32dee253fa1821344efe
Eduard Kutuev <nap@ajamstudio.com>
2011-06-08 04:22:18 +0400

    Добавлен скрипт для пересоздания баз в текущем проекте.

--- fw/bin/sql-init.sh
+++ fw/bin/sql-init.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
 
 BASEPATH=`dirname $0`/../..
-PLUGINS=$BASEPATH/plugins
 
-for plugin in `ls $PLUGINS`; do
-	name="$PLUGINS/$plugin/bin/db.sql";
-	if [ -f $name ]; then
-		cat $name
-		echo ""
+sql=`$BASEPATH/fw/bin/sql-getall.sh`
+
+sites=`ls $BASEPATH/sites`
+echo This script will delete all database data for next projects: $sites
+echo This server is `hostname`.
+read -p "Type YES to continue: "
+if [[ ! $REPLY =~ ^[Yy][Ee][Ss]$ ]] ; then
+	echo Cancelled.
+	exit 1
+fi
+
+for site in $BASEPATH/sites/*; do
+	if [ -f $site/config.php ]; then
+		db=`echo "<?php
+			\\$config = include( '$site/config.php' );
+			echo \\$config['db']['username'];
+			echo ' ';
+			echo \\$config['db']['password'];
+			echo ' ';
+			echo \\$config['db']['database'];" | php`
+		user=`echo $db|awk '{print \$1}'`
+		pass=`echo $db|awk '{print \$2}'`
+		base=`echo $db|awk '{print \$3}'`
+
+		tables=`mysql -u$user -p$pass $base -e 'show tables'|awk '{ print $1}'|grep -v '^Tables'`
+		for t in $tables; do
+			mysql -u$user -p$pass $base -e "drop table $t"
+		done
+
+		mysql -u$user -p$pass $base -e "$sql"
+	else
+		echo "WARNING: Can't find $site/config.php"
 	fi
 done
 
+echo Done.
