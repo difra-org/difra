d683a53f322c9b2fc40f6cfbba08f8099e33fb9d
pnd <pnd@a-jam.ru>
2011-06-10 22:09:03 +0000

    Добавлены функции возвращающие дополнительные поля из базы данных.

--- fw/lib/additionals.php
+++ fw/lib/additionals.php
@@ -59,5 +59,44 @@ class Additionals {
 			}
 		}
 	}
+	
+	/**
+	 * Additionals::getAdditionals()
+	 * @desc Возвращает массив с дополнительными полями
+	 * @param string $module
+	 * @param integer $id
+	 * @return array || boolean
+	 */
+	public static function getAdditionals( $module, $id ) {
+
+		$db = MySQL::getInstance();
+		$query = "SELECT `name`, `value` FROM `{$module}_fields` WHERE `id`='" . $db->escape( $id ) . "'";
+		$res = $db->fetch( $query );
+		$fields = array();
+		foreach( $res as $k=>$data ) {
+			$fields[$data['name']] = $data['value'];
+		}
+		return !empty( $fields ) ? $fields : false;
+	}
+	
+	/**
+	 * Additionals::getAdditionalsXml()
+	 * @desc Добавляет xml с дополнительными полями
+	 * @param string $module
+	 * @param integer $id
+	 * @param DOMNode $node
+	 * @return void
+	 */
+	public static function getAdditionalsXml( $module, $id, $node ) {
+
+		if( $data = self::getAdditionals( $module, $id ) ) {
+			$addNode = $node->appendChild( $node->ownerDocument->createElement( $module . 'Additionals' ) );
+			if( !empty( $data ) ) {
+				foreach( $data as $k => $v ) {
+					$addNode->setAttribute( $k, $v );
+				}
+			}
+		}
+	}
 
 }	
