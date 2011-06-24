2778f1dc7e859cd0976c022735b19a342d324aaa
Eduard Kutuev <nap@ajamstudio.com>
2011-06-24 20:02:03 +0400

    Теперь сначала инициализируются таблицы плагинов, а потом сайта

--- fw/bin/sql-getall.sh
+++ fw/bin/sql-getall.sh
@@ -3,13 +3,8 @@
 BASEPATH=`dirname $0`/../..
 PLUGINS=$BASEPATH/plugins
 
-name="$BASEPATH/bin/db.sql";
-if [ -f $name ]; then
-	cat $name
-	echo ""
-fi
-
 echo "SET FOREIGN_KEY_CHECKS = 0;"
+
 for plugin in `ls $PLUGINS`; do
 	name="$PLUGINS/$plugin/bin/db.sql";
 	if [ -f $name ]; then
@@ -17,4 +12,11 @@ for plugin in `ls $PLUGINS`; do
 		echo ""
 	fi
 done
+
+name="$BASEPATH/bin/db.sql";
+if [ -f $name ]; then
+	cat $name
+	echo ""
+fi
+
 echo "SET FOREIGN_KEY_CHECKS = 1;"
