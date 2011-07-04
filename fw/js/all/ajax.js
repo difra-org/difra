dbff1cfb3514a7bc10a1889fdf53008bac282c6c
Eduard Kutuev <nap@ajamstudio.com>
2011-07-04 22:15:20 +0400

    Добавлена поддержка ajax-запросов с произввольными заголовками, исправлен баг

--- fw/js/all/ajax.js
+++ fw/js/all/ajax.js
@@ -17,7 +17,7 @@ var main = {
 	context: this
 };
 
-main.httpRequest = function( url, params ) {
+main.httpRequest = function( url, params, headers ) {
 
 	var data = {};
 	if( typeof params == 'undefined' ) {
@@ -25,6 +25,9 @@ main.httpRequest = function( url, params ) {
 	} else {
 		data.type = 'POST';
 		data.data = 'json=' + JSON.stringify( params );
+	}
+	if( typeof headers != 'undefined' ) {
+		data.headers = headers;
 	}
 	return $.ajax( url, data ).responseText;
 };
