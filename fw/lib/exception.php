7972810eba4f8b946c7c7d480c83d183f01e118f
Eduard Kutuev <nap@ajamstudio.com>
2011-09-18 02:28:48 +0400

    Улучшена информативность уведомлений об исключениях

--- fw/lib/exception.php
+++ fw/lib/exception.php
@@ -7,16 +7,31 @@ class Exception extends \exception {
 	public function __construct( $message = null, $code = 0, \Exception $previous = null ) {
 
 		$date = date( 'r' );
+		$server = print_r( $_SERVER, true );
+		$post = print_r( $_POST, true );
+		$cookie = print_r( $_COOKIE, true );
+		$user = Auth::getInstance()->data['email'];
+
 		$text = <<<MSG
 $message
 
+Page:	{$_SERVER['REQUEST_URI']}
 Time:	$date
 Host:	{$_SERVER['HTTP_HOST']}
 File:		{$this->getFile()}
 Line:	{$this->getLine()}
+User:	$user
 
 {$this->getTraceAsString()}
 
+\$_SERVER:
+$server
+
+\$_POST:
+$post
+
+\$_COOKIE:
+$cookie
 MSG;
 
 		if( !Debugger::getInstance()->isEnabled() ) {
