ca087488e9729e1f6ebab4257d1caae3a58e6c45
Eduard Kutuev <nap@ajamstudio.com>
2011-09-18 02:13:59 +0400

    Добавлено уведомление на почту об exception'ах в production mode.

--- fw/lib/exception.php
+++ fw/lib/exception.php
@@ -3,5 +3,25 @@
 namespace Difra;
 
 class Exception extends \exception {
-	
+
+	public function __construct( $message = null, $code = 0, \Exception $previous = null ) {
+
+		$date = date( 'r' );
+		$text = <<<MSG
+$message
+
+Time:	$date
+Host:	{$_SERVER['HTTP_HOST']}
+File:		{$this->getFile()}
+Line:	{$this->getLine()}
+
+{$this->getTraceAsString()}
+
+MSG;
+
+		if( !Debugger::getInstance()->isEnabled() ) {
+			mail( 'errors@a-jam.ru', 'Report from ' . $_SERVER['HTTP_HOST'], $text );
+			echo 'Error.';
+		}
+	}
 }
