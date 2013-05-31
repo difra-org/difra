1c1dfa0c10bf6dedb300c07a795a656d594951fa
Eduard Kutuev <nap@ajamstudio.com>
2013-05-31 10:56:51 +0400

    jQueryUI merged into trunk

--- plugins/jqueryui/plugin.php
+++ plugins/jqueryui/plugin.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace Difra\Plugins\jqueryui;
+
 class Plugin extends \Difra\Plugin {
 
-	protected $version = 3.1;
+	protected $version = 4;
 	protected $description = 'jQuery UI library';
 
 	public function init() {
-    }
+	}
 }
 
