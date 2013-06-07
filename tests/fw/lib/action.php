83ba01340a85ff9832f15cce6cafcf949135df1b
Eduard Kutuev <nap@ajamstudio.com>
2013-06-08 00:27:26 +0400

    Проверка работоспособности assertEquals для объектов в тестах.

--- tests/fw/lib/action.php
+++ tests/fw/lib/action.php
@@ -2,10 +2,18 @@
 
 class ActionTest extends PHPUnit_Framework_TestCase {
 
+	public function test_getInstance() {
+
+		$action1 = \Difra\Action::getInstance();
+		$action2 = \Difra\Action::getInstance();
+		$this->assertEquals( $action1, $action2 );
+	}
+
 	public function test_getUri_Fail() {
 
 		$this->setExpectedException( 'Difra\Exception' );
 		$action = new \Difra\Action;
 		$action->getUri();
 	}
+
 }
