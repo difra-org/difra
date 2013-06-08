e4c4fd9a2d9251e54e7d5585f4afdcce5216cc76
Eduard Kutuev <nap@ajamstudio.com>
2013-06-09 02:18:20 +0400

    Добавлен тест загрузки Unify-структуры.

---
+++ tests/fw/lib/unify.php
@@ -0,0 +1,21 @@
+<?php
+
+class UnifyTest extends PHPUnit_Framework_TestCase {
+
+	public function test_get() {
+
+		include_once( __DIR__ . '/unify/test.inc' );
+		\Difra\Unify::registerObjects( array( 'test' => 'TestObj' ) );
+		$test = \Difra\Unify::getObj( 'test', 10 ); // проверка кэша
+		$test2 = TestObj::get( 10 );
+		$this->assertSame( $test, $test2 );
+		$this->assertEquals( $test::getTable(), 'test_table' );
+		$this->assertEquals( $test::getPrimary(), 'id' );
+		$this->assertEquals( $test->getPrimaryValue(), 10 );
+		$this->assertEquals( $test->getKeys( false ), array( 'id', 'visible', 'parent', 'test2', 'title' ) );
+		$this->assertEquals( $test->getKeys( false ), array( 'id', 'visible', 'parent', 'test2', 'title' ) ); // ещё раз для проверки кэшей
+		$this->assertEquals( $test->getKeys( true ), array( 'id', 'visible', 'parent', 'test2', 'title', 'description' ) );
+		$this->assertEquals( $test->getKeys( 'only' ), array( 'description' ) );
+		$this->assertEquals( $test->getDefaultSearchConditions(), array( 'visible' => 1 ) );
+	}
+}
