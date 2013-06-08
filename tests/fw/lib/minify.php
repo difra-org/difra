0699643546accdc02fe0bbdb74f5b9bf04a407dc
Eduard Kutuev <nap@ajamstudio.com>
2013-06-08 23:46:37 +0400

    И вдогонку — правильные данные для теста.

--- tests/fw/lib/minify.php
+++ tests/fw/lib/minify.php
@@ -47,12 +47,7 @@ body {
 
 EOT
 		);
-		$this->assertEquals( $test,
-			<<<EOT
-
-html{min-width:980px;}body{position:absolute;left:0;right:0;top:0;bottom:0;}
-EOT
-		);
+		$this->assertEquals( $test, "html{min-width:980px;}body{position:absolute;left:0;right:0;top:0;bottom:0;}" );
 	}
 
 	public function test_none() {
