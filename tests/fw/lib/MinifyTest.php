f3efea9c561341873e4e3ff32e97103c03794350
Eduard Kutuev <nap@ajamstudio.com>
2013-06-10 13:29:00 +0400

    MinifyTest доработан для 100% покрытия тестами.

--- tests/fw/lib/MinifyTest.php
+++ tests/fw/lib/MinifyTest.php
@@ -16,6 +16,7 @@ test.test = function( e ) {
 	( 5 - 7 ) / 1;
 	testValue4
 	= '';
+	testValue4 = testValue4.(/[\\\\]/);
 	return Form.Validator.getValidator('IsEmpty').test(element) || (/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]\.?){0,63}[a-z0-9!#$%&'*+/=?^_`{|}~-]@(?:(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)*[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\])$/i).test(element.get('value'));};
 
 EOT
@@ -24,9 +25,33 @@ EOT
 			<<<EOT
 
 var test={};test.test=function(e){test.testValue=e;testValue2='abcd\\'ed';testValue3=3-4+
-(5-7)/1;testValue4='';return Form.Validator.getValidator('IsEmpty').test(element)||(/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]\.?){0,63}[a-z0-9!#$%&'*+/=?^_`{|}~-]@(?:(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)*[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\])$/i).test(element.get('value'));};
+(5-7)/1;testValue4='';testValue4=testValue4.(/[\\\\]/);return Form.Validator.getValidator('IsEmpty').test(element)||(/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]\.?){0,63}[a-z0-9!#$%&'*+/=?^_`{|}~-]@(?:(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)*[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\])$/i).test(element.get('value'));};
 EOT
 		);
+	}
+
+	public function test_InvalidJS() {
+
+		$this->setExpectedException( 'Difra\Exception' );
+		\Difra\Minify::getInstance( 'js' )->minify( 'var ab = \'' );
+	}
+
+	public function test_InvalidJS2() {
+
+		$this->setExpectedException( 'Difra\Exception' );
+		\Difra\Minify::getInstance( 'js' )->minify( 'var ab = /* djaisjdio' );
+	}
+
+	public function test_InvalidJS3() {
+
+		$this->setExpectedException( 'Difra\Exception' );
+		\Difra\Minify::getInstance( 'js' )->minify( "return t = testValue4.(/^)" );
+	}
+
+	public function test_InvalidJS4() {
+
+		$this->setExpectedException( 'Difra\Exception' );
+		\Difra\Minify::getInstance( 'js' )->minify( "return t = testValue4.(/[abc/);" );
 	}
 
 	public function test_css() {
