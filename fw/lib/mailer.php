21a07de6a72098e3551b3ab8a241445b75afdd0a
Eduard Kutuev <nap@ajamstudio.com>
2013-06-26 13:20:56 +0400

    В отправляемые письма добавлен заголовок Message-Id.

--- fw/lib/mailer.php
+++ fw/lib/mailer.php
@@ -2,6 +2,11 @@
 
 namespace Difra;
 
+/**
+ * Class Mailer
+ *
+ * @package Difra
+ */
 class Mailer {
 
 	var $fromText = 'Robot';
@@ -27,12 +32,11 @@ class Mailer {
 
 	/**
 	 * Отправляет письмо
-	 * @param string        $email                To:
-	 * @param string        $subject              Subject:
-	 * @param string        $body                 Тело письма
-	 * @param string|bool   $fromMail             From: (адрес)
-	 * @param string|bool   $fromText             From: (имя)
-	 *
+	 * @param string      $email                To:
+	 * @param string      $subject              Subject:
+	 * @param string      $body                 Тело письма
+	 * @param string|bool $fromMail             From: (адрес)
+	 * @param string|bool $fromText             From: (имя)
 	 * @return bool
 	 * @throws exception
 	 */
@@ -49,10 +53,11 @@ class Mailer {
 			"Mime-Version: 1.0",
 			"Content-Type: text/html; charset=\"UTF-8\"",
 			"Date: " . date( 'r' ),
+			"Message-Id: " . md5( microtime() ) . '-' . md5( $fromMail . $email ) . '@' . Envi::getHost( true ),
 			'Content-Transfer-Encoding: 8bit',
 			'From: =?utf-8?B?' . base64_encode( $fromText ) . "==?= <{$fromMail}>"
 		);
-		$subj    = '=?utf-8?B?' . base64_encode( $subject ) . '==?=';
+		$subj = '=?utf-8?B?' . base64_encode( $subject ) . '==?=';
 		if( !mail( $email, $subj, $body, implode( "\r\n", $headers ) ) ) {
 			throw new exception( "Failed to send message to $email." );
 		}
@@ -71,17 +76,17 @@ class Mailer {
 	 */
 	public function createMail( $email, $template, $data ) {
 
-		$xml  = new \DOMDocument();
+		$xml = new \DOMDocument();
 		$root = $xml->appendChild( $xml->createElement( 'mail' ) );
 		$this->addData( $root, $data );
-		$view         = new View;
+		$view = new View;
 		$templateText = $view->render( $xml, $template, true );
 
 		preg_match( '|<subject[^>]*>(.*)</subject>|Uis', $templateText, $subject );
 		preg_match( '|<text[^>]*>(.*)</text>|Uis', $templateText, $mailText );
 		preg_match( '|<from[^>]*>(.*)</from>|Uis', $templateText, $fromMail );
 		preg_match( '|<fromtext[^>]*>(.*)</fromtext>|Uis', $templateText, $fromText );
-		$subject  = !empty( $subject[1] ) ? $subject[1] : '';
+		$subject = !empty( $subject[1] ) ? $subject[1] : '';
 		$mailText = !empty( $mailText[1] ) ? $mailText[1] : '';
 		$fromMail = !empty( $fromMail[1] ) ? $fromMail[1] : $this->fromMail;
 		$fromText = !empty( $fromText[1] ) ? $fromText[1] : $this->fromText;
@@ -97,7 +102,7 @@ class Mailer {
 	 */
 	private function addData( $node, $data ) {
 
-		$node->setAttribute( 'host', Site::getInstance()->getMainhost() );
+		$node->setAttribute( 'host', Envi::getHost( true ) );
 		Locales::getInstance()->getLocaleXML( $node );
 		foreach( $data as $k => $v ) {
 			$node->setAttribute( $k, $v );
