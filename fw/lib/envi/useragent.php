15280ebfdfda03a55d7cb03264085c790f1db27b
Eduard Kutuev <nap@ajamstudio.com>
2013-07-01 22:14:00 +0400

    Рефакторинг Difra\Envi\UserAgent.

--- fw/lib/envi/useragent.php
+++ fw/lib/envi/useragent.php
@@ -26,7 +26,7 @@ class UserAgent {
 		'Trident' => 'Trident'
 	);
 
-	static $os = array(
+	static $oses = array(
 		'Windows' => 'Windows',
 		'Macintosh' => 'Macintosh',
 		'iPad' => 'iOS',
@@ -44,86 +44,187 @@ class UserAgent {
 	 */
 	public static function getUserAgentXML( $node ) {
 
-		$ua = self::getUserAgent();
-		if( $ua ) {
+		if( $ua = self::getUserAgent() ) {
 			DOM::array2domAttr( $node, $ua );
 		}
-		$uac = self::getUserAgentClass();
-		if( $uac ) {
+		if( $uac = self::getUserAgentClass() ) {
 			$node->setAttribute( 'uaClass', $uac );
 		}
 	}
 
+	private static $userAgent = null;
+
 	/**
 	 * Возвращает массив с данными о браузере пользователя
-	 * @return array|bool
+	 * @return array
 	 */
 	public static function getUserAgent() {
 
-		static $userAgent = null;
-		if( !is_null( $userAgent ) ) {
-			return $userAgent;
+		if( !is_null( self::$userAgent ) ) {
+			return self::$userAgent;
 		}
-		if( empty( $_SERVER['HTTP_USER_AGENT'] ) ) {
-			$userAgent = false;
-			return $userAgent;
+		return self::$userAgent = array(
+			'agent' => self::getAgent(),
+			'version' => self::getVersion(),
+			'os' => self::getOS(),
+			'engine' => self::getEngine()
+		);
+	}
+
+	private static $agentId = null;
+
+	/**
+	 * Возвращает id браузера, как он называет себя
+	 *
+	 * @return string|bool
+	 */
+	public static function getAgentId() {
+
+		if( !is_null( self::$agentId ) ) {
+			return self::$agentId;
 		}
-		// разбиваем строку User-Agent на ассоциативный массив
-		preg_match_all( '/([^\/]+)\/([^\/]+)(\s|$)/', $ua0 = $_SERVER['HTTP_USER_AGENT'], $ua1 );
+		$ua = self::getUAArray();
+		foreach( self::$agents as $agent => $aName ) {
+			if( isset( $ua[$agent] ) ) {
+				return self::$agentId = $agent;
+			}
+		}
+		return self::$agentId = false;
+	}
+
+	private static $agent = null;
+
+	/**
+	 * Возвращает имя браузера
+	 * @return string|bool
+	 */
+	public static function getAgent() {
+
+		if( !is_null( self::$agent ) ) {
+			return self::$agent;
+		}
+		if( $agentId = self::getAgentId() and isset( self::$agents[$agentId] ) ) {
+			return self::$agent = self::$agents[$agentId];
+		}
+		$ua = self::getUAArray();
+		if( isset( $ua['Mozilla'] ) and strpos( $ua['Mozilla'], 'MSIE' ) ) {
+			return self::$agentId = 'IE';
+		}
+	}
+
+	private static $engine = null;
+
+	/**
+	 * Возвращает движок браузера
+	 * @return string|bool
+	 */
+	public static function getEngine() {
+
+		if( !is_null( self::$engine ) ) {
+			return self::$engine;
+		}
+		$ua = self::getUAArray();
+		foreach( self::$engines as $engine => $eName ) {
+			if( isset( $ua[$engine] ) ) {
+				return self::$engine = $eName;
+			}
+		}
+		return self::$engine = false;
+	}
+
+	private static $os = null;
+
+	/**
+	 * Возвращает операционную систему пользователя
+	 * @return string|bool
+	 */
+	public static function getOS() {
+
+		if( !is_null( self::$os ) ) {
+			return self::$os;
+		}
+		$uaString = self::getUAString();
+		foreach( self::$oses as $os => $osName ) {
+			if( strpos( $uaString, $os ) ) {
+				return self::$os = $osName;
+			}
+		}
+		return self::$os = false;
+	}
+
+	private static $version = null;
+
+	/**
+	 * Возвращает версию браузера
+	 * @return string|bool
+	 */
+	public static function getVersion() {
+
+		if( !is_null( self::$version ) ) {
+			return self::$version;
+		}
+		$ua = self::getUAArray();
+		if( isset( $ua['Version'] ) ) {
+			return self::$version = $ua['Version'];
+		}
+		$agentId = self::getAgentId();
+		if( isset( $ua[$agentId] ) ) {
+			if( $agentId == 'Opera' ) {
+				return self::$version = explode( ' ', $ua[$agentId], 2 )[0];
+			} else {
+				return self::$version = $ua[$agentId];
+			}
+		}
+		$agent = self::getAgent();
+		if( $agent == 'IE' ) {
+			$version = substr( $ua['Mozilla'], strpos( $ua['Mozilla'], 'MSIE' ) + 4 );
+			if( $p = strpos( $version, ';' ) ) {
+				$version = substr( $version, 0, $p );
+			}
+			return self::$version = trim( $version );
+		}
+		return self::$version = false;
+	}
+
+	private static $uaString = null;
+
+	/**
+	 * Возвращает строку User-Agent
+	 *
+	 * @return string|bool
+	 */
+	public static function getUAString() {
+
+		if( !is_null( self::$uaString ) ) {
+			return self::$uaString;
+		}
+		if( isset( $_SERVER['HTTP_USER_AGENT'] ) ) {
+			return self::$uaString = $_SERVER['HTTP_USER_AGENT'];
+		}
+		return self::$uaString = false;
+	}
+
+	private static $uaArray = null;
+
+	/**
+	 * Возвращает ассоциативный массив, сформированный из строки User-Agent
+	 *
+	 * @return array
+	 */
+	private static function getUAArray() {
+
+		if( !is_null( self::$uaArray ) ) {
+			return self::$uaArray;
+		}
 		$ua = array();
+		preg_match_all( '/([^\/]+)\/([^\/]+)(\s|$)/', self::getUAString(), $ua1 );
 		foreach( $ua1[1] as $k => $v ) {
 			$ua[$ua1[1][$k]] = $ua1[2][$k];
 		}
-		$a = array(
-			'agent' => '',
-			'version' => '',
-			'os' => '',
-			'engine' => ''
-		);
-		// определяем название браузера
-		if( isset( $ua['Mozilla'] ) and strpos( $ua['Mozilla'], 'MSIE' ) ) {
-			$a['agent'] = 'IE';
-		} else {
-			foreach( self::$agents as $agent => $aName ) {
-				if( isset( $ua[$agent] ) ) {
-					$a['agent'] = $aName;
-					$a['agentId'] = $agent;
-					break;
-				}
-			}
-		}
-		// определяем движок
-		foreach( self::$engines as $engine => $eName ) {
-			if( isset( $ua[$engine] ) ) {
-				$a['engine'] = $eName;
-				break;
-			}
-		}
-		// определяем версию
-		if( isset( $ua['Version'] ) ) {
-			$a['version'] = $ua['Version'];
-		} elseif( isset( $ua[$a['agentId']] ) ) {
-			$a['version'] = $ua[$a['agentId']];
-			if( $a['agent'] == 'Opera' ) {
-				$a['version'] = explode( ' ', $a['version'], 2 )[0];
-			}
-		} elseif( $a['agent'] == 'IE' and isset( $ua['Mozilla'] ) and strpos( $ua['Mozilla'], 'MSIE' ) ) {
-			$a['version'] = substr( $ua['Mozilla'], strpos( $ua['Mozilla'], 'MSIE' ) + 4 );
-			if( $p = strpos( $a['version'], ';' ) ) {
-				$a['version'] = substr( $a['version'], 0, $p );
-			}
-			$a['version'] = trim( $a['version'] );
-		}
-		// определяем платформу
-		foreach( self::$os as $os => $osName ) {
-			if( strpos( $ua0, $os ) ) {
-				$a['os'] = $osName;
-				break;
-			}
-		}
-		unset( $a['agentId'] );
-		return $userAgent = $a;
+		return self::$uaArray = $ua;
 	}
+
+	private static $uaClass = null;
 
 	/**
 	 * Возвращает строку для CSS-классов, основанных на версии браузера
@@ -132,14 +233,10 @@ class UserAgent {
 	 */
 	public static function getUserAgentClass() {
 
-		static $uaClass = null;
-		if( !is_null( $uaClass ) ) {
-			return $uaClass;
+		if( !is_null( self::$uaClass ) ) {
+			return self::$uaClass;
 		}
 		$a = self::getUserAgent();
-		if( empty( $a ) ) {
-			return $uaClass = '';
-		}
 		$uac = array();
 		if( $a['agent'] ) {
 			$uac[] = $a['agent'];
@@ -154,6 +251,19 @@ class UserAgent {
 		if( $a['engine'] ) {
 			$uac[] = $a['engine'];
 		}
-		return $uaClass = trim( implode( ' ', $uac ) );
+		return self::$uaClass = trim( implode( ' ', $uac ) );
+	}
+
+	public static function setUAString( $string ) {
+
+		self::$uaString = $string;
+		self::$userAgent = null;
+		self::$agent = null;
+		self::$agentId = null;
+		self::$version = null;
+		self::$engine = null;
+		self::$os = null;
+		self::$uaArray = null;
+		self::$uaClass = null;
 	}
 }
