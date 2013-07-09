4728ce0b4e6c62a1b93b6dd956b8038773f8fbbb
Eduard Kutuev <nap@ajamstudio.com>
2013-07-09 16:09:04 +0400

    Фикс ViewTest.

--- tests/fw/lib/ViewTest.php
+++ tests/fw/lib/ViewTest.php
@@ -4,22 +4,20 @@ class ViewTest extends PHPUnit_Framework_TestCase {
 
 	public function test_render_dontEcho() {
 
-		$view = new \Difra\View;
 		$xml = new \DOMDocument;
 		$realRoot = $xml->appendChild( $xml->createElement( 'root' ) );
 		$realRoot->appendChild( $xml->createElement( 'content' ) );
-		$html = $view->render( $xml, 'main', true );
+		$html = \Difra\View::render( $xml, 'main', true );
 		$this->assertNotEmpty( $html );
 	}
 
 	public function test_render_echo() {
 
-		$view = new \Difra\View;
 		$xml = new \DOMDocument;
 		$realRoot = $xml->appendChild( $xml->createElement( 'root' ) );
 		$realRoot->appendChild( $xml->createElement( 'content' ) );
 		ob_start();
-		$view->render( $xml, 'main' );
+		\Difra\View::render( $xml, 'main' );
 		$html = ob_get_clean();
 		$this->assertNotEmpty( $html );
 	}
