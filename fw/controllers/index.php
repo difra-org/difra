ad61ba236939c620222c8154dc2c3ff1eb2fb9a0
Eduard Kutuev <nap@ajamstudio.com>
2013-07-20 14:06:45 +0400

    В дефолтный indexController->indexAction() добавлено создание XML-элемента /root/content/index для упрощения вёрстки простых сайтов, не требующих программирования.

--- fw/controllers/index.php
+++ fw/controllers/index.php
@@ -1,7 +1,12 @@
 <?php
 
+/**
+ * Class IndexController
+ */
 class IndexController extends \Difra\Controller {
 
 	public function indexAction() {
+
+		$this->root->appendChild( $this->xml->createElement( 'index' ) );
 	}
 }
