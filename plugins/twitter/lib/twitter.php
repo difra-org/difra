e42bc77fa34590b8e52176a992c42070af748b67
pnd <pnd@a-jam.ru>
2013-10-25 11:30:11 +0000

    Добавлен плагин для работы с Twitter API

---
+++ plugins/twitter/lib/twitter.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Difra\Plugins;
+
+class Twitter {
+
+	private $errorCode = 0;
+	private $errorMessage = null;
+	private $response = null;
+
+	/**
+	 * Адрес на который будет отправлен запрос на создание твита
+	 * @var string
+	 */
+	private static $postUrl = 'https://api.twitter.com/1.1/statuses/update.json';
+
+	/**
+	 * Постинг сообщения в твиттер
+	 * @param $text
+	 */
+	public static function post( $text ) {
+
+		$requestFields = array( 'status' => $text );
+		$Twitter = new self;
+		$Twitter->_performRequest( $requestFields );
+
+		return $Twitter;
+	}
+
+	/**
+	 * Выполняет запрос
+	 * @param array  $fields
+	 * @param string $method
+	 */
+	private function _performRequest( array $fields, $method = 'POST' ) {
+
+		$OAuth = \Difra\Plugins\Twitter\Oauth::build( self::$postUrl );
+		$header = $OAuth->getHeader();
+
+		$options = array(
+			CURLOPT_HTTPHEADER => $header,
+			CURLOPT_HEADER => false,
+			CURLOPT_URL => self::$postUrl,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_TIMEOUT => 15,
+		);
+
+		if( $method == 'POST' ) {
+			$options[CURLOPT_POSTFIELDS] = $fields;
+		} else {
+			$options[CURLOPT_URL] .= $fields;
+		}
+
+		$feed = curl_init();
+		curl_setopt_array( $feed, $options );
+		$jsonAnswer = curl_exec( $feed );
+		curl_close( $feed );
+
+		if( !empty( $jsonAnswer ) ) {
+			$this->response = json_decode( $jsonAnswer, true );
+		}
+
+		$this->_getErrors( $jsonAnswer );
+
+	}
+
+	/**
+	 * Возвращает код ошибки, если она случилась
+	 * @return int
+	 */
+	public function getErrorCode() {
+		return $this->errorCode;
+	}
+
+	/**
+	 * Возвращает текст ошибки.
+	 * @return null
+	 */
+	public function getErrorMessage() {
+		return $this->errorMessage;
+	}
+
+	/**
+	 * Возвращает ответ сервера после запроса
+	 * @return null
+	 */
+	public function getResponse() {
+		return $this->response;
+	}
+
+	private function _getErrors( $jsonAnswer ) {
+
+		if( isset( $this->response['errors'] ) && is_array( $this->response['errors'] ) ) {
+
+			foreach( $this->response['errors'] as $k => $data ) {
+				if( isset( $data['message'] ) ) {
+					$this->errorMessage = $data['message'];
+				}
+				if( isset( $data['code'] ) ) {
+					$this->errorCode = intval( $data['code'] );
+				}
+			}
+		}
+	}
+
+}
+
---
+++ plugins/twitter/xslt/adm/adm_twitter_settings.xsl
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns="http://www.w3.org/1999/xhtml" version="1.0">
+
+	<xsl:template match="twitterSettings">
+
+		<h2>
+			<xsl:value-of select="$locale/twitter/adm/oAuth/settingsTitle"/>
+		</h2>
+
+		<form name="oauth_settings_form" method="post" action="/adm/settings/twitter/savesettings/" class="ajaxer">
+
+			<table class="form">
+				<colgroup>
+					<col style="width: 250px"/>
+					<col/>
+				</colgroup>
+				<tr>
+					<th>
+						<xsl:value-of select="$locale/twitter/adm/oAuth/consumerKey"/>
+					</th>
+					<td>
+						<input type="text" name="consumerKey" class="full-width" value="{@consumerKey}" />
+					</td>
+				</tr>
+				<tr>
+					<th>
+						<xsl:value-of select="$locale/twitter/adm/oAuth/consumerSecret"/>
+					</th>
+					<td>
+						<input type="text" name="consumerSecret" class="full-width" value="{@consumerSecret}" />
+					</td>
+				</tr>
+				<tr>
+					<th>
+						<xsl:value-of select="$locale/twitter/adm/oAuth/oAuthtoken"/>
+					</th>
+					<td>
+						<input type="text" name="oauthToken" class="full-width" value="{@accessToken}" />
+					</td>
+				</tr>
+				<tr>
+					<th>
+						<xsl:value-of select="$locale/twitter/adm/oAuth/oAuthsecret"/>
+					</th>
+					<td>
+						<input type="text" name="oauthSecret" class="full-width" value="{@accessTokenSecret}" />
+					</td>
+				</tr>
+			</table>
+
+			<input type="submit" value="{$locale/adm/save}"/>
+
+		</form>
+
+	</xsl:template>
+</xsl:stylesheet>
