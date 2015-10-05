7a829e3fc967890f3d1b7f62e01a148d7c1664b6
Eduard Kutuev <nap@ajamstudio.com>
2015-10-05 13:29:49 +0300

    Added Difra\Param\NamedPaginator parameter type

---
+++ fw/lib/param/namedpaginator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Difra\Param;
+use Difra\Unify\Paginator;
+
+/**
+ * Class NamedInt
+ * @package Difra\Param
+ */
+class NamedPaginator extends Common
+{
+    const source = 'query';
+    const type = 'int';
+    const named = true;
+    const auto = true;
+
+    /** @noinspection PhpMissingParentConstructorInspection
+     * Constructor
+     * @param string $value
+     */
+    public function __construct($value = '')
+    {
+        if ($value === '') {
+            $value = 1;
+        }
+        $this->value = new Paginator();
+        $this->value->setPage($value);
+    }
+
+    public function __call($method, $args)
+    {
+        call_user_func_array(array($this->value, $method), $args);
+    }
+}
