6a8b7d427972834b2e60c585541e85980d22a01f
Eduard Kutuev <nap@ajamstudio.com>
2015-11-17 10:08:28 +0300

    Difra\Cache: fixed MemCache and MemCached adapters. Fixed Autoloader unit test to match new paths.

--- tests/fw/lib/AutoloaderTest.php
+++ tests/fw/lib/AutoloaderTest.php
@@ -11,9 +11,9 @@ class AutoloaderTest extends PHPUnit_Framework_TestCase
 
     public function test_paths()
     {
-        $this->assertEquals(\Difra\Autoloader::class2file('Test'), DIR_ROOT . 'lib/test.php');
-        $this->assertEquals(\Difra\Autoloader::class2file('Difra\\Test'), DIR_FW . 'lib/test.php');
-        $this->assertEquals(\Difra\Autoloader::class2file('Difra\\Plugins\\Test'), DIR_PLUGINS . 'test/lib/test.php');
-        $this->assertEquals(\Difra\Autoloader::class2file('Difra\\Plugins\\Test\\A'), DIR_PLUGINS . 'test/lib/a.php');
+        $this->assertEquals(\Difra\Autoloader::class2file('Test'), DIR_ROOT . 'lib/Test.php');
+        $this->assertEquals(\Difra\Autoloader::class2file('Difra\\Test'), DIR_FW . 'lib/Test.php');
+        $this->assertEquals(\Difra\Autoloader::class2file('Difra\\Plugins\\Test'), DIR_PLUGINS . 'Test/lib/Test.php');
+        $this->assertEquals(\Difra\Autoloader::class2file('Difra\\Plugins\\Test\\A'), DIR_PLUGINS . 'Test/lib/A.php');
     }
 }
