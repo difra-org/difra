90d2082b3b5c24fe8e164d60f9f103a956fd16d8
Eduard Kutuev <nap@ajamstudio.com>
2016-04-13 18:42:58 +0300

    Added Difra\Locker component

---
+++ fw/lib/Locker.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Difra;
+
+/**
+ * Class Locker
+ * @package Difra
+ *
+ * Use locks to prevent race conditions.
+ */
+class Locker
+{
+    /** Cache prefix */
+    const PREFIX = 'lock:';
+    /** Lock try timeout, seconds */
+    const TIMEOUT = 10;
+    /** Prevent cache rewrite race condition delay, microseconds */
+    const DELAY_S = 10000;
+    /** Waiting for lock release delay, microseconds */
+    const DELAY_L = 50000; // microseconds
+    /** Maximum lock release wait time */
+    const TTL = 5; // seconds
+
+    /** @var string */
+    private $key = null;
+    /** @var int */
+    private $rnd = null;
+
+    /**
+     * Lock
+     * @param string $skey
+     * @return Locker
+     * @throws \Exception
+     */
+    public static function create($skey)
+    {
+        $rnd = mt_rand(100000000, 999999999);
+        $key = self::PREFIX . $skey;
+        $cache = Cache::getInstance();
+        if ($cache->adapter == Cache::INST_NONE) {
+            return new self();
+        }
+        $started = microtime(true);
+        while (1) {
+            $state = $cache->get($key);
+            if (!$state) {
+                $cache->put($key, $rnd, self::TTL);
+                usleep(self::DELAY_S);
+            } elseif ($state == $rnd) {
+                $lock = new self;
+                $lock->key = $key;
+                $lock->rnd = $rnd;
+                return $lock;
+            } else {
+                usleep(self::DELAY_L);
+            }
+            if (microtime(true) - $started > self::TTL) {
+                throw new \Exception('Failed to make lock');
+            }
+        }
+    }
+
+    /**
+     * Unlock
+     */
+    public function remove()
+    {
+        if ($this->key and Cache::getInstance()->get($this->key) == $this->rnd) {
+            Cache::getInstance()->remove($this->key);
+            $this->key = null;
+        }
+    }
+
+    /**
+     * Locker constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    public function __destruct()
+    {
+        $this->remove();
+    }
+}
