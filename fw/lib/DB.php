6390a13e73ad38bd85cbe7c950967de19d6e57fa
Eduard Kutuev <nap@ajamstudio.com>
2016-10-14 15:52:21 +0300

    Added SQLite support

--- fw/lib/DB.php
+++ fw/lib/DB.php
@@ -3,6 +3,7 @@
 namespace Difra;
 
 use Difra\DB\Adapters\MySQL;
+use Difra\DB\Adapters\Sqlite;
 
 /**
  * Factory for DB
@@ -33,6 +34,8 @@ class DB
         switch (strtolower($cfg[$instance]['type'])) {
             case 'mysql':
                 return self::$adapters[$instance] = new MySQL($cfg[$instance]);
+            case 'sqlite':
+                return self::$adapters[$instance] = new Sqlite($cfg[$instance]);
             default:
                 throw new Exception("PDO adapter not found for '{$cfg[$instance]['type']}'");
         }
@@ -51,41 +54,41 @@ class DB
 
         $cfg = Config::getInstance()->get('db');
 
-        // generate default config + backwards compatibility
-        if (empty($cfg) or empty($cfg['default'])) {
-            $cfg['default'] = [];
-        }
-        $keys = ['type', 'hostname', 'database', 'username', 'password'];
-        foreach ($keys as $key) {
-            if (!isset($cfg['default'][$key])) {
-                if (isset($cfg[$key])) {
-                    $cfg['default'][$key] = $cfg[$key];
-                    unset($cfg[$key]);
-                } else {
-                    switch ($key) {
-                        case 'type':
-                            $cfg['default']['type'] = 'mysql';
-                            break;
-                        case 'database':
-                        case 'username':
-                            $cfg['default'][$key] = Envi::getSubsite();
-                            break;
-                        default:
-                            $cfg['default'][$key] = '';
-                    }
-                }
-            }
-        }
-
-        // add missing keys from default config
-        foreach ($cfg as $name => &$conf) {
-            foreach ($keys as $key) {
-                $conf['name'] = $name;
-                if (!isset($conf[$key])) {
-                    $conf[$key] = $cfg['default'][$key];
-                }
-            }
-        }
+//        // generate default config + backwards compatibility
+//        if (empty($cfg) or empty($cfg['default'])) {
+//            $cfg['default'] = [];
+//        }
+//        $keys = ['type', 'hostname', 'database', 'username', 'password'];
+//        foreach ($keys as $key) {
+//            if (!isset($cfg['default'][$key])) {
+//                if (isset($cfg[$key])) {
+//                    $cfg['default'][$key] = $cfg[$key];
+//                    unset($cfg[$key]);
+//                } else {
+//                    switch ($key) {
+//                        case 'type':
+//                            $cfg['default']['type'] = 'mysql';
+//                            break;
+//                        case 'database':
+//                        case 'username':
+//                            $cfg['default'][$key] = Envi::getSubsite();
+//                            break;
+//                        default:
+//                            $cfg['default'][$key] = '';
+//                    }
+//                }
+//            }
+//        }
+//
+//        // add missing keys from default config
+//        foreach ($cfg as $name => &$conf) {
+//            foreach ($keys as $key) {
+//                $conf['name'] = $name;
+//                if (!isset($conf[$key])) {
+//                    $conf[$key] = $cfg['default'][$key];
+//                }
+//            }
+//        }
 
         return $cfg;
     }
