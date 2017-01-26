0fcb86540352d1bcf9e9705ddb6cc9d397207da7
Eduard Kutuev <nap@napych.com>
2017-01-26 15:18:39 +0300

    Removed old-plugins

--- old-plugins/catalog/js/main/orders.js
+++
@@ -1,7 +0,0 @@
-/*
-function openOrderEdit( id, open ) {
-	$( '#order-edit-hidden-' + id ).css( 'display', open ? 'none' : 'inline' );
-	$( '#order-edit-visible-' + id ).css( 'display', open ? 'inline' : 'none' );
-	$( '#order-edit-form-' + id ).css( 'display', open ? 'block' : 'none' );
-}
-	*/
