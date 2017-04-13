2ea3c513485245045168a4f670dd7239c915b7eb
den <denis.vinokurov@gmail.com>
2017-04-13 14:38:22 +0300

    Do not use switcher if default click action was prevented

--- js/all/switcher.js
+++ js/all/switcher.js
@@ -112,6 +112,9 @@ switcher.page = function (url, noPush, data) {
 
 switcher.bind = function () {
     $(document).on('click dblclick touchend', 'a', function (event) {
+        if (event.isDefaultPrevented()) {
+            return;
+        }
 
         // skip .ajaxer and .noAjaxer links
         if ($(this).hasClass('ajaxer') || $(this).hasClass('noAjaxer')) {
