a6561b7421f159ad17e37fdfac75d8c763106729
den <denis.vinokurov@gmail.com>
2017-10-24 17:04:04 +0000

    Check for xml errors

--- src/Resourcer/Abstracts/XML.php
+++ src/Resourcer/Abstracts/XML.php
@@ -2,6 +2,8 @@
 
 namespace Difra\Resourcer\Abstracts;
 
+use Difra\Exception;
+
 /**
  * Abstract adapter for XML resources
  */
@@ -20,7 +22,17 @@ abstract class XML extends Common
         $newXml = new \SimpleXMLElement("<{$this->type}></{$this->type}>");
         foreach ($files as $file) {
             $filename = $withFilenames ? $file['raw'] : null;
+            $old = libxml_use_internal_errors(true);
             $xml = simplexml_load_file($file['raw']);
+            if ($xml === false) {
+                $message = '';
+                foreach (libxml_get_errors() as $error) {
+                    $message .= $this->createErrorMessage($error) . PHP_EOL;
+                }
+                libxml_use_internal_errors($old);
+                throw new Exception($message);
+            }
+            libxml_use_internal_errors($old);
             $this->mergeXML($newXml, $xml, $filename);
             foreach ($xml->attributes() as $key => $value) {
                 $newXml->addAttribute($key, $value);
@@ -31,6 +43,26 @@ abstract class XML extends Common
             $this->postprocess($newXml, $instance);
         }
         return $newXml->asXML();
+    }
+
+    /**
+     * Create error message from LibXMLError object
+     * @param \LibXMLError $error
+     * @return string
+     */
+    private function createErrorMessage(\LibXMLError $error)
+    {
+        $type = 'error (unknown type)';
+        if ($error->level === \LIBXML_ERR_WARNING) {
+            $type = 'warning';
+        }
+        if ($error->level === \LIBXML_ERR_ERROR) {
+            $type = 'error';
+        }
+        if ($error->level === \LIBXML_ERR_FATAL) {
+            $type = 'fatal error';
+        }
+        return sprintf('libxml %s %s: %s in file %s (%s)', $type, $error->code, trim($error->message), $error->file, $error->line);
     }
 
     /**
