acf254151fcf582a0bc33eb9af42054479ad17ea
Eduard Kutuev <ed@napych.com>
2020-08-27 17:09:48 +0300

    adm 404 pages fix

--- controllers/adm/index.php
+++ controllers/adm/index.php
@@ -2,10 +2,16 @@
 
 namespace Controller\Adm;
 
+use Difra\Controller;
+use Difra\View\HttpError;
+
 class Index extends \Difra\Controller\Adm
 {
     public function indexAction()
     {
+        if (Controller::hasUnusedParameters()) {
+            throw new HttpError(404);
+        }
         \Difra\View::redirect('/adm/status');
     }
 }
