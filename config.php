be84267742a179b4e95f10176cf1f45961ac61e4
Eduard Kutuev <ed@napych.com>
2020-11-02 20:50:42 +0300

    Moving adm interface to difra/adm package

--- config.php
+++ config.php
@@ -4,53 +4,42 @@ return [
     'instances' => [
         'main' => [
             'include' => [
-                'jquery3' => true,
-                'bootstrap' => true,
+//                'jquery3' => true,
+//                'bootstrap' => true,
                 'all' => true,
-                'switcher' => true
+//                'switcher' => true
             ]
         ],
-        'adm' => [
-//            'compatibility' => 6,
-            'include' => [
-                'jquery3' => true,
-//                'switcher' => true,
-                'bootstrap' => true,
-                'all' => true,
-                'fontawesome5' => true
-            ]
-        ],
-        'all' => [
-            'include' => [
-                'jquery3' => true
-            ]
-        ],
-        'bootstrap' => [
-            'include' => [
-		'popper' => true,
-		'jquery' => true
-            ]
-        ],
-        'popper' => [
-            'include' => [
-                'jquery' => true
-            ]
-        ],
-        'console' => [
-            'include' => [
-                'jquery' => true
-            ]
-        ],
-        'switcher' => [
-            'include' => [
-                'jquery' => true
-//                'websymbols' => true
-            ]
-        ],
-        'bootstrap-select' => [
-            'include' => [
-                'bootstrap' => true
-            ]
-        ]
+//        'all' => [
+//            'include' => [
+//                'jquery3' => true
+//            ]
+//        ],
+//        'bootstrap' => [
+//            'include' => [
+//                'popper' => true,
+//                'jquery' => true
+//            ]
+//        ],
+//        'popper' => [
+//            'include' => [
+//                'jquery' => true
+//            ]
+//        ],
+//        'console' => [
+//            'include' => [
+//                'jquery' => true
+//            ]
+//        ],
+//        'switcher' => [
+//            'include' => [
+//                'jquery' => true
+//            ]
+//        ],
+//        'bootstrap-select' => [
+//            'include' => [
+//                'bootstrap' => true
+//            ]
+//        ]
     ]
 ];
